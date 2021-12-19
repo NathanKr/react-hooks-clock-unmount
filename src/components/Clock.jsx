94b68addc3420b5e60e832657a840c3d68bc3401
Nathan Krasney <natankrasney@gmail.com>
2021-12-19 14:14:01 +0200

    ...

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -2,11 +2,16 @@ import React, { useEffect } from "react";
 import moment from "moment";
 const Clock = () => {
   useEffect(() => {
-    handle = setInterval(writeTime, 500);
+    handle = setInterval(writeTime, 500); // --- this is called at component mount --> []
+    console.log('useEffect is called'); // --- this is called at component mount --> []
     return unmountHandler; // --- this is called at component unmount
-  });
+  },[]);
 
   let handle = null;
+
+  function mountHandler(){
+      
+  }
 
   function unmountHandler() {
     console.log("unmount phase - clock");
